808955a5fa3fa90c3094816247de8ec21251d119
abdullashahzan <abdullah.getstac@gmail.com>
2024-03-25 01:34:53 +0300

    Version 2.2

--- ecom/ecom/urls.py
+++ ecom/ecom/urls.py
@@ -19,7 +19,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include("shop.urls"))
+    path('', include("shop.urls")),
 ]
 
 handler404 = 'shop.views.page_404'
